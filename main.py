fb6712b223fe231d4cf23658f868da5cfea5d0ec
Édrihan <96261928+edrihan@users.noreply.github.com>
2023-02-24 03:59:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import tkinter as tk
 tokens = {
     'AGIXBUSD': {
         'low_trigger': 0.35,
-        'high_trigger': 0.42,
+        'high_trigger': 0.43,
     },
    }
 # Define the interval (in seconds)
-interval = 30
+interval = 20
 api_url = 'https://api.binance.com/api/v3/ticker/price'
+last_info = None
+
 def check_data():
     for token in tokens:
         params = {'symbol': token}
@@ -22,27 +24,44 @@ def check_data():
         now = datetime.datetime.now()
         # Format the date and time as a string
         date_string = now.strftime("%Y-%m-%d %H:%M:%S")
-        assert low_trigger < high_trigger, f'I think you meant different numbers because low_trigger: {low_trigger} > high_trigger: {high_trigger}
-        print(f"{date_string} The current price of {token} is {price} USDT")
+        assert low_trigger < high_trigger, f'I think you meant different numbers because low_trigger: {low_trigger} > high_trigger: {high_trigger}'
+        info = f"{date_string} The current price of {token} is {price} USDT"
+        print(info)
+        try:
+            global message, last_info
+            if last_info != info:
+                message.config(text=info)
+                last_info = info
+        except UnboundLocalError as e:
+            
+            print(e,"Can't update Main Window's message because it doesn't exist")
+            # Create a label with a message to display
+            #message = tk.Label(root, text="Init ,message")
+        
         if price > high_trigger or price < low_trigger:
-            # Create a new window
+            # Create a poput to indicate trigger is reached
             popup = tk.Tk()
-            # Set the title of the window
             popup.title(f"{token} alert.")
             # Create a label with a message to display
             message = tk.Label(popup, text=f"{token} has hit {price},\nwhich is outside the non-triggering range of {low_trigger} - {high_trigger}.")
-            # Add the label to the window
+            # Add the label to the root
             message.pack()
             # Start the GUI event loop
             popup.mainloop()
+        root.after(interval, check_data)
+# Create a root window
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+# Create the Tkinter root and set its initial resolution
+root.geometry("400x300")
+root.title("Main Window")
+# Create a label with a message to display
+message = tk.Label(root, text="Info about shit\nAnd...")
+# Add the label to the root
+message.pack()
 
-# Create a scheduler object
-scheduler = sched.scheduler(time.time, time.sleep)
-# Schedule the function to be executed every 'interval' seconds
-def schedule_next_execution():
-    scheduler.enter(interval, 1, schedule_next_execution)
-    check_data()
+root.after(interval, check_data)
 
-# Start the scheduler
-schedule_next_execution()
-scheduler.run()
+# Start the GUI event loop
+root.mainloop()
